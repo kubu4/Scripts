d001be3a2e16c34cf0dbcab4f11233fc9508eb8a
Sam <samuel.j.white@gmail.com>
2016-02-04 16:05:49 -0800

    adjusted sed sub in line 2

--- bash/ngs_automator.sh
+++ bash/ngs_automator.sh
@@ -1,3 +1,3 @@
 
 #Finds folders lacking readme.md files, creates them, and writes current directory path to readme.md file
-while read i; do echo "$i"; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/readme.md" ';' -print | sed 's/Volumes\/owl_web/owl\/web/')
+while read i; do echo "$i" > "$i"/readme.md; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/readme.md" ';' -print | sed 's/Volumes\/owl_web/web/')
