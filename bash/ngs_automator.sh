acd66f6fc001a39121dac8d3794ca067d611bb39
kubu4 <samuel.j.white@gmail.com>
2016-02-04 20:52:54 -0800

    Comment out lines for testing.

--- bash/ngs_automator.sh
+++ bash/ngs_automator.sh
@@ -4,9 +4,11 @@
 #and appends the name and corresponding read counts to the newly created readme.md files.
 
 #Finds folders lacking readme.md files and stores to variable "no_readme"
+
 no_readme=$(while read i; do echo "$i"; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/readme.md" ';' -print | sed 's/Volumes\/owl_web/web/'))
 
 #Writes path to readme files in directories lacking readme files.
+
 printf '%s\n' "$no_readme" | while IFS= read -r line; do echo "$line" >> "$line"/readme.md; done
 
 
@@ -15,9 +17,11 @@ printf '%s\n' "$no_readme" | while IFS= read -r line; do echo "$line" >> "$line"
 #Illumina sequencing files are composed of four lines per read.
 #Format the output (printf) to print the filename, followed by a tab, followed by the readcount.
 #The command "tee -a" is used to both print the output to the screen and append the output to the readme.md file.
-printf '%s\n' "$no_readme"  | while IFS= read -r line; do linecount=`gunzip -c "$line"/*.gz | wc -l`; readcount=$(($linecount/4)); printf "%s\t%s\n\n" "${line##*/}" "$readcount" >> "$line"/readme.md;
-done
+
+#printf '%s\n' "$no_readme"  | while IFS= read -r line; do linecount=`gunzip -c "$line"/*.gz | wc -l`; readcount=$(($linecount/4)); printf "%s\t%s\n\n" "${line##*/}" "$readcount" >> "$line"/readme.md;
+#done
 
 #Finds folders lacking checksums.md5 files and generates checksums for all files in that directory
-while read i; do md5 "$i"/*.gz >> checksums.md5; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/checksums.md5" ';' -print)
+
+#while read i; do md5 "$i"/*.gz >> checksums.md5; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/checksums.md5" ';' -print)
 
