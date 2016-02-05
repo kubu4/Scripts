a178de1a6b0b587380b16abbabd8f743dc958aa5
Sam <samuel.j.white@gmail.com>
2016-02-04 16:10:35 -0800

    add checksum locator/creator

--- bash/ngs_automator.sh
+++ bash/ngs_automator.sh
@@ -1,3 +1,5 @@
-
 #Finds folders lacking readme.md files, creates them, and writes current directory path to readme.md file
 while read i; do echo "$i" > "$i"/readme.md; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/readme.md" ';' -print | sed 's/Volumes\/owl_web/web/')
+
+#Finds folders lacking checksums.md5 files and generates checksums for all files in that directory
+while read i; do md5 "$i"/*.gz >> checksums.md5; done < <(find "`pwd`" -not -path '*/\.*' -mindepth 1 -type d '!' -exec test -e "{}/checksums.md5" ';' -print)
