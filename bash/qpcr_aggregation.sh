4b9020616c63c393dd894cbf73865980be35997c
Sam White <samuel.j.white@gmail.com>
2016-05-05 08:33:00 -0700

    add section comments as outline

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-# Shell script to aggregate and format exported data from BioRad CFX Manager v.3.x
+# Shell script to aggregate and format exported CSV data from BioRad CFX Manager v.3.x.
+
+# Dependencies:
+# R script: CFX_Cq_Agg.R (https://github.com/kubu4/Scripts/blob/master/R/CFX_Cq_Agg.R)
+
+
+# Remove spaces from filenames
+
+# Add date to first column
+
+# Append new first column with filename.
+
+# Run Rscript (CFX_Cq_Agg.R) to combine replicate Cq data on single line
+
+# 
