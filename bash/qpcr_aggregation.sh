f0026f7e8900b6ccb965d0966520c768fec654e9
Sam White <samuel.j.white@gmail.com>
2016-05-05 10:18:54 -0700

    add quotes around array variables

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -15,8 +15,8 @@ for file in *.csv; do
 	OIFS="$IFS"
 	IFS="_"
 	read -a qpcr_date <<< "${file}"
-	qpcr_date=${qpcr_date[1]}
-	echo ${qpcr_date[1]}
+	qpcr_date="${qpcr_date[1]}"
+	echo "${qpcr_date[1]}"
 	IFS="$OIFS"
 
 # Add date to first column
