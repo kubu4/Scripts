f222815efad1168942a94560bc68e836b64c9abb
Sam White <samuel.j.white@gmail.com>
2016-05-05 08:34:43 -0700

    initiate for loop; add mv to rename files

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -5,13 +5,13 @@
 # Dependencies:
 # R script: CFX_Cq_Agg.R (https://github.com/kubu4/Scripts/blob/master/R/CFX_Cq_Agg.R)
 
-
+for file in . do
 # Remove spaces from filenames
-
+	do mv "$file" ${file// /}
 # Add date to first column
 
 # Append new first column with filename.
 
 # Run Rscript (CFX_Cq_Agg.R) to combine replicate Cq data on single line
 
-# 
+done
