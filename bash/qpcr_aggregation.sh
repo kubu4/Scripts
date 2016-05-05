9049813ea5eaca82dd9900d0cc52a792341d1f7e
Sam White <samuel.j.white@gmail.com>
2016-05-05 12:00:13 -0700

    add old_head variable to store header info

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -13,6 +13,12 @@
 #	mv "$file" "${file// /_}"
 #done
 
+# Create new header with qPCR_date as first column name
+
+old_head="$(,Well,Fluor,Target,Content,Sample,Biological Set Name,Cq,Cq Mean,Cq Std. Dev,Starting Quantity (SQ),Log Starting Quantity,SQ Mean,SQ Std. Dev,Set Point,Well Note
+)"
+echo "$old_head"
+
 # Pull date from filename
 for file in *.csv; do
 	OIFS="$IFS"
@@ -22,7 +28,10 @@ for file in *.csv; do
 	echo "$qpcr_date"
 	IFS="$OIFS"
 
-# Add date to first column
+# Add qPCR_date header to first column
+
+	
+# Add qPCR date to first column
 	awk -v var="$qpcr_date" '{ print var$0 }' "$file" > "${file/.csv/.tmp}"
 
 # Append new first column with filename.
