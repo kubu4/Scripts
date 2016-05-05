d7e03d6cf8e8306e0c5b671882e759e89a532d75
Sam White <samuel.j.white@gmail.com>
2016-05-05 10:08:30 -0700

    uncomment date pull commands

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -12,9 +12,9 @@ for file in *.csv; do
 	mv "$file" "${file// /_}"
 	
 # Pull date from filename
-	#IFS="_"; read -ra qpcr_date <<< "$file"
-	#qpcr_date=${qpcr_date[1]}
-	#echo ${qpcr_date[1]}
+	IFS="_"; read -ra qpcr_date <<< "$file"
+	qpcr_date=${qpcr_date[1]}
+	echo ${qpcr_date[1]}
 # Add date to first column
 	#awk -F, '{$1=1 FS $1;}1' OFS=,
 
