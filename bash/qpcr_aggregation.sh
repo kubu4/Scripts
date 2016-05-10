45eea6e9e176fe6b26645cef6d8d5528ea5ca12e
Sam White <samwhite@uw.edu>
2016-05-10 15:41:17 -0700

    comment out last for loop and sed commands

--- bash/qpcr_aggregation.sh
+++ bash/qpcr_aggregation.sh
@@ -85,9 +85,9 @@ for file in *Quantification*.csv; do
 	### Add new first column and append filename to .tmp files created in previous step.
 	### Pass bash variable ($qpcr_filename) to awk, and append value to new column.
 	### Concatenate output to master.csv file.
-	for file2 in *.tmp; do
-		awk -F, -v var="$qpcr_filename" '{$1=var FS $1;}1' OFS=, "$file2" >> "$master_list"
-	done
+	######for file2 in *.tmp; do
+		######awk -F, -v var="$qpcr_filename" '{$1=var FS $1;}1' OFS=, "$file2" >> "$master_list"
+	#######done
 done
 
 	
@@ -95,5 +95,5 @@ done
 ## Takes $master_list (.csv file) as input.
 ## Use sed to edit $master_list "in place" and create a backup file with .old extension (-i.old).
 ## Sed inserts $new_head above the first line of $master_list and then deletes the backup file.
-sed -i.old "1s/^.*$/$new_head/" "$master_list"
-rm *.old
+######sed -i.old "1s/^.*$/$new_head/" "$master_list"
+######rm *.old
