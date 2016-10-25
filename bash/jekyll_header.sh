4b0a60ef0de40e71d3446206390a5af54a6a4bf7
Sam <samuel.j.white@gmail.com>
2016-10-25 08:39:14 -0700

    cleaned up script and rm various echo test statements

--- bash/jekyll_header.sh
+++ bash/jekyll_header.sh
@@ -19,17 +19,12 @@ read PHRASE
 echo "You entered $PHRASE"
 
 #Remove spaces from PHRASE and replace with hyphens
-
 FORMATTED_PHRASE="$(echo -ne "${PHRASE}" | tr [:space:] '-')"
-
-echo "$FORMATTED_PHRASE"
-#Create new file
-touch "$NEW_MD_FILE"
 
 
 # Prints formatted Jekyll header utilizing POST_DATE and user-entered PHRASE.
+# Writes contents to NEW_MD_FILE
 printf "%s\n%s\n%s_%s_\n%s'%s\'\n%s\n" "$MD_LINE" "$LAYOUT" "$TITLE" "$PHRASE" "$DATE_LINE" "$POST_DATE" "$MD_LINE" >> \
 $NEW_MD_FILE
 
-cat $NEW_MD_FILE
 
