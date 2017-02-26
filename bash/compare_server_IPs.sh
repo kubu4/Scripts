69aa2fb5b16ca7636d9f42037324bcbdb2fda601
kubu4 <samuel.j.white@gmail.com>
2017-02-25 19:51:11 -0800

    Add CURRENT_IP variable.
    Store IP address from wget statement.

--- bash/compare_server_IPs.sh
+++ bash/compare_server_IPs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -q -O - checkip.dyndns.com | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+CURRENT_IP=$(wget -q -O - checkip.dyndns.com | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
