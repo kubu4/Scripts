bfaeeaf5334d2bf544bae005315fc04234076648
kubu4 <samuel.j.white@gmail.com>
2017-02-26 10:15:44 -0800

    Add outline for script.

--- bash/compare_server_IPs.sh
+++ bash/compare_server_IPs.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 CURRENT_IP=$(wget -q -O - checkip.dyndns.com | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+
+OUTLINE
+
+# Determine if file containing IP address exists
+
+# If not, create the file and add the current IP address to the file.
+
+# Else, grab current IP address and store to a diffferent file
+
+# Compare the two files.
+
+# If two files are different, mail IP address to me.
