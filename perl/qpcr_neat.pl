54bcced83d723b5d58d4351b3262c3d5bcfc3150
sam <samwhite@uw.edu>
2017-05-03 09:36:31 -0700

    update comments on column append

--- perl/qpcr_neat.pl
+++ perl/qpcr_neat.pl
@@ -40,10 +40,12 @@ while (my $line = <$in>) {
     # Spliting the columns at each comma
     my @data = split(/,/, $line);
 
+    #Save value last index in array to variable
+    my $add_column = $#data;
+    
     # Append to current line if Content (column 6) and Sample (column 7) 
     # on one line equals Content and Sample on the next line,
     # otherwise set our looping variables to new ones
-    my $add_column = $#data;
     if ($curr_content eq $data[5] && $curr_sample eq $data[6]) {
         $curr_line = $curr_line . "," . $data[$add_column];
     } else {
