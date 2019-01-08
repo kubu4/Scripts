1d4aafa17ed3999621469547f2b8698fe4e275a6
Sam White <samuel.j.white@gmail.com>
2019-01-08 08:45:25 -0800

    change work dir

--- sbatch/20190108_oly_maker_interproscan.sh
+++ sbatch/20190108_oly_maker_interproscan.sh
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=samwhite@uw.edu
 ## Specify the working directory for this job
-#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190107_oly_maker_interproscan
+#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_interproscan
 
 # Load Python Mox module for Python module availability
 
